d7ad7ee308aac6f9e66e02981c4ebee5cddd4829
mercury-pluto1210 <mercury.pluto1210@gmail.com>
2020-01-19 02:12:42 +0900

    first commit

---
+++ About/script.js
@@ -0,0 +1,30 @@
+$(function(){
+    $('.bars-text').hover(
+        function(){
+            $('.bars-text').css('background-color','rgba(102, 101, 101, 1.0)');
+        },
+        function(){
+            $('.bars-text').css('background-color','rgba(102, 101, 101, 0.7)');
+        }
+    );
+    $('.heart').click(function(){
+        if($('.heart').hasClass('like')){
+            $('.heart').removeClass('like');
+            $('.like-contents').show().slideUp('fast');  
+        }
+        else{
+            $('.heart').addClass('like');
+            $('.like-contents').slideDown('fast').show();     
+        }
+    });
+    $('.bars-text').click(function(){
+        $('.menu').animate({
+            'right':'0%'
+        },1000);
+    });
+    $('.back').click(function(){
+        $('.menu').animate({
+            'right':'-23%'
+        },1000);
+    });
+});
